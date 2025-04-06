2e05c91a0c6a155f1bfb1585939955033e3a8fb4
Zerefez <127219749+Zerefez@users.noreply.github.com>
2025-04-06 13:56:06 +0200

    Delete AuthServer directory

--- AuthServer/DTOs/AuthResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace AuthServer.DTOs
-{
-    public class AuthResponseDto
-    {
-        public bool Success { get; set; }
-        public UserDto? User { get; set; }
-        public string? Error { get; set; }
-    }
-} 
