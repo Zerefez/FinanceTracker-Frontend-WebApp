dd9bbae69f68eafb063d733f010390273d35963a
Zerefez <202209849@post.au.dk>
2025-05-18 19:20:23 +0200

    run

--- show-coverage.bat
+++ show-coverage.bat
@@ -8,7 +8,7 @@ echo.
 set NODE_OPTIONS=--max-old-space-size=4096
 
 REM Simple command to match exactly what's shown in the terminal
-call node node_modules/vitest/vitest.mjs run --coverage
+call node node_modules/vitest/vitest.mjs run --coverage --reporter=verbose
 
 echo.
 echo ==========================================
